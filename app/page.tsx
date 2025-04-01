6ddc0e6ce97224f1d1a729bc9d65a82e46270fcf
System Administrator <root@192.168.1.9>
2025-04-01 14:37:44 +0000

    Ajout page.tsx pour App Router

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main style={{ padding: "100px", textAlign: "center" }}>
+      <h1>Bienvenue chez Zuterion Corp 🎉</h1>
+      <p>Le site est bien en ligne !</p>
+    </main>
+  );
+}
